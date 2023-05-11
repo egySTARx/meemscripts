ca47f3a5ca499dcfeb7da61d467e23bdb3ec5793
egySTARx <ma6eek@gmail.com>
2023-05-11 14:48:43 +0300

    Update meemdeb.sh

--- meemdeb.sh
+++ meemdeb.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+curl -o webmin.sh https://raw.githubusercontent.com/egySTARx/meemscripts/main/webmin.sh
 sh setup-repos.sh
 apt install webmin
-curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
+#curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
+curl -s https://install.zerotier.com | sudo bash
+
