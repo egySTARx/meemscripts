009729aaefb4141ad95c8a864b0c93906f90c3ad
egySTARx <ma6eek@gmail.com>
2023-05-11 15:39:01 +0300

    Update meemdeb.sh

--- meemdeb.sh
+++ meemdeb.sh
@@ -4,5 +4,8 @@ curl -o webmin.sh https://raw.githubusercontent.com/egySTARx/meemscripts/main/we
 sh setup-repos.sh
 apt install webmin
 #curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
+hostnamectl set-hostname MeeMVPN
 curl -s https://install.zerotier.com | sudo bash
+apt install net-tools
+apt install iptables -y
 
