cd39443cbe5ed13ee91dd6625a1b506c9f3d0dd4
egySTARx <ma6eek@gmail.com>
2023-05-11 20:38:28 +0300

    Update meemdeb.sh

--- meemdeb.sh
+++ meemdeb.sh
@@ -2,10 +2,10 @@
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 curl -o webmin.sh https://raw.githubusercontent.com/egySTARx/meemscripts/main/webmin.sh
 sh webmin.sh
-apt install webmin
+apt install webmin -y
 #curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
 hostnamectl set-hostname MeeMVPN
 curl -s https://install.zerotier.com | sudo bash
-apt install net-tools
+apt install net-tools -y
 apt install iptables -y
 
