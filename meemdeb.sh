a91b870b92e9dc61e647c4a903a8358408835288
egySTARx <ma6eek@gmail.com>
2023-05-11 20:32:50 +0300

    Update meemdeb.sh

--- meemdeb.sh
+++ meemdeb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 curl -o webmin.sh https://raw.githubusercontent.com/egySTARx/meemscripts/main/webmin.sh
-sh setup-repos.sh
+sh webmin.sh
 apt install webmin
 #curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
 hostnamectl set-hostname MeeMVPN
