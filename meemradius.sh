a9c619f88641d0982b728a48e7956a39fe2c0323
egySTARx <ma6eek@gmail.com>
2023-05-21 12:24:51 +0300

    Update meemradius.sh

--- meemradius.sh
+++ meemradius.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget http://file.7yro.net/meem/radius/technoradius.sh.x
-chmod +x /root/technoradius.sh.x
-/root/technoradius.sh.x
+#wget http://file.7yro.net/meem/radius/technoradius.sh.x
+#chmod +x /root/technoradius.sh.x
+#/root/technoradius.sh.x
 echo TechnoMeeM MaGeek egySTARx 01027698925
-rm -rf /root/technoradius.sh.x
+#rm -rf /root/technoradius.sh.x
