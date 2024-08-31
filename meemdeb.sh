f13a6173b64085bcbf3cb6dc843134d26f3dc070
MaGeek egySTARx <ma6eek@gmail.com>
2024-08-31 04:39:03 +0300

    Update meemdeb.sh

--- meemdeb.sh
+++ meemdeb.sh
@@ -3,9 +3,11 @@ sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd
 curl -o webmin.sh https://raw.githubusercontent.com/egySTARx/meemscripts/main/webmin.sh
 sh webmin.sh
 apt install webmin -y
-sudo apt -y install open-vm-tools ntpdate sntp
+#sudo apt -y install open-vm-tools ntpdate sntp
 hostnamectl set-hostname localhost
-apt install -y net-tools iptables
+#apt install -y net-tools iptables
 curl -s https://install.zerotier.com | sudo bash
 zerotier-cli join 632ea290853efff7
 #curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
+
+#curl -fsSL https://get.casaos.io | sudo bash
