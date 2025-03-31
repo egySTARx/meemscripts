bdd0eb69e9f167b0769adbf8655e63015048b42a
MaGeek egySTARx <ma6eek@gmail.com>
2025-03-31 21:35:38 +0300

    Update meemdeb.sh

--- meemdeb.sh
+++ meemdeb.sh
@@ -4,10 +4,10 @@ curl -o webmin.sh https://raw.githubusercontent.com/egySTARx/meemscripts/main/we
 sh webmin.sh
 apt install webmin -y
 #sudo apt -y install open-vm-tools ntpdate sntp
-hostnamectl set-hostname localhost
+#hostnamectl set-hostname localhost
 #apt install -y net-tools iptables
 curl -s https://install.zerotier.com | sudo bash
-zerotier-cli join 632ea290853efff7
+#zerotier-cli join 632ea290853efff7
 #curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
 
 #curl -fsSL https://get.casaos.io | sudo bash
